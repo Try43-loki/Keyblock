2732dec72de4f1a7afaf6423871c2fe5af47ecdd
Tryy-43 <meysoytry@gmail.com>
2024-09-08 10:05:54 +0700

    keyblock

--- public/swiper.js
+++ public/swiper.js
@@ -1,12 +1,94 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
+const swiper = new Swiper(".slider-wrapper", {
   loop: true,
-  effect: "cube",
   grabCursor: true,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-nextt",
+    prevEl: ".swiper-button-prevv",
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+const swiperPromotion = new Swiper(".slider-wrapper-promotion", {
+  loop: true,
+  grabCursor: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next-promotion",
+    prevEl: ".swiper-button-prev-promotion",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+const swiperCustomer = new Swiper(".slider-wrapper-customer", {
+  loop: true,
+  grabCursor: true,
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next-promotion",
+  //   prevEl: ".swiper-button-prev-promotion",
+  // },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+const heroSection = new Swiper(".hero-section", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    dynamicBullets: true,
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next-slider",
+    prevEl: ".swiper-button-prev-slider",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
 });
